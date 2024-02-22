b6c4b71f01eddc108d28bade1a475cb48f8159e7
yossefabc <yossefbekele30@gmail.com>
2024-02-22 12:06:39 +0200

    main

--- hakhaton/app.js
+++ hakhaton/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('express');
 const app = express();
+const router = express.Router();
 const knex = require('knex');
 const db = knex({
     client:'pg',
@@ -15,9 +16,32 @@ const db = knex({
 })
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("okkk")
+});
+
+app.post('/register', async (req, res) => {
+    const { first_name, last_name, email, username, password } = req.body;
+    try {
+        await db("users")
+        .insert({first_name:first_name,last_name:last_name,email:email,username:username,password:password})
+         res.sendFile(__dirname+'/index/index.html')
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal server error');
+    }
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
